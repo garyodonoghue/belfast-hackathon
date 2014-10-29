bdcd7b72c7ac1482b1f5960ce951266ff8507583
witnessmenow <ecenuig@gmail.com>
2014-10-29 02:16:47 +0000

    All MCP controller code now in this code base

--- core/src/main/java/com/ladinc/core/BelfastGC.java
+++ core/src/main/java/com/ladinc/core/BelfastGC.java
@@ -1,5 +1,7 @@
 package com.ladinc.core;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
@@ -9,6 +11,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ladinc.core.assets.Assets;
 import com.ladinc.core.contorllers.MyControllerManager;
 import com.ladinc.core.screens.GameScreen;
+import com.ladinc.mcp.MCP;
+import com.ladinc.mcp.RedirectOption;
 
 public class BelfastGC extends Game {
 	public static float delta;
@@ -29,6 +33,20 @@ public class BelfastGC extends Game {
 	{
 		Assets.load();
 		this.mcm = new MyControllerManager();
+		
+		this.mcm.moreControllers.customLinks = new ArrayList<String>();
+		this.mcm.moreControllers.customLinks.add("Background.png");
+		this.mcm.moreControllers.customLinks.add("post.png");
+		this.mcm.moreControllers.customLinks.add("target.png");
+		this.mcm.moreControllers.customLinks.add("transStick.png");
+		this.mcm.moreControllers.customLinks.add("postman.html");
+		
+		this.mcm.moreControllers.redirectOptions = new ArrayList<RedirectOption>();
+		this.mcm.moreControllers.redirectOptions.add(new RedirectOption("postman.html", "Postman"));
+		
+		MCP.SHOW_DEBUG_LOGGING = false;
+		
+		
 		createScreens();
 		setScreen(gameScreen);
 	}
